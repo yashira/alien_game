3b7a87da9c605c307d31bbe6094199ca2470ece0
Yashira <yashira.perera@novigi.com.au>
2022-10-06 12:49:00 +0530

    Changing mongodb uri

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = process.env.mongodb_uri;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(
   uri,
